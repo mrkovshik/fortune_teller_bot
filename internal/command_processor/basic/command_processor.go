57a94783a670696018de0c4149ac0bf106a62865
N.Kulakov <nkulakov@stsoft.ru>
2025-08-02 15:32:20 +0200

    fixed commands

--- internal/command_processor/basic/command_processor.go
+++ internal/command_processor/basic/command_processor.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/mrkovshik/fortune_teller_bot/internal/command_processor"
 	"github.com/mrkovshik/fortune_teller_bot/internal/storage"
 	"github.com/mrkovshik/fortune_teller_bot/internal/storage/local"
 	"go.uber.org/zap"
@@ -22,13 +23,13 @@ func NewCommandProcessor(logger *zap.SugaredLogger) *CommandProcessor {
 
 func (cp *CommandProcessor) ProcessCommand(command string) (string, error) {
 	switch command {
-	case "list_books": //TODO: make const
+	case command_processor.ListBooksCommandName:
 		list, err := cp.storage.ListBooks()
 		if err != nil {
 			return "", fmt.Errorf(`failed to list books: %w`, err)
 		}
 		return strings.Join(list, "/n"), nil // TODO: use template
-	case "get_magic":
+	case command_processor.GetMagicCommandName:
 		return cp.storage.GetRandomSentenceFromBook(local.DorianGreyTitle)
 	default:
 		return "Неизвестная команда", nil
---
+++ internal/command_processor/commands.go
@@ -0,0 +1,6 @@
+package command_processor
+
+const (
+	GetMagicCommandName  = "/get_magic"
+	ListBooksCommandName = "/list_books"
+)
